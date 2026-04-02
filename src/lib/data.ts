c760365485c7561f284685b9413575255461d39e
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-04-02 19:01:07 +0100

    feat: central data

---
+++ src/lib/data.ts
@@ -0,0 +1,25 @@
+export const SOCIALS = {
+  github: {
+    name: "GitHub",
+    handle: "@muizzyranking",
+    href: "https://github.com/Muizzyranking",
+  },
+  linkedin: {
+    name: "LinkedIn",
+    handle: "Muiz Oyebowale",
+    href: "https://linkedin.com/in/muizzyrankin",
+  },
+  twitter: {
+    name: "Twitter / X",
+    handle: "@muizzyranking",
+    href: "https://twitter.com/muizzyranking",
+  },
+  hashnode: {
+    name: "Hashnode",
+    handle: "muizzyranking.hashnode.dev",
+    href: "https://muizzyranking.hashnode.dev",
+  },
+};
+
+export const EMAIL = "oyebowaleabdulmuiz@gmail.com"
+
