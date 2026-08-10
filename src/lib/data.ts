010b2a213f9aacad564f9fc13bf7bc09a756af18
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-08-10 01:09:10 +0100

    feat: add sage theme, Poppins typeface, motion tokens, and site config

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,19 +1,9 @@
-export const SOCIALS = {
-  github: {
-    name: "GitHub",
-    handle: "@muizzyranking",
-    href: "https://github.com/Muizzyranking",
-  },
-  linkedin: {
-    name: "LinkedIn",
-    handle: "Muiz Oyebowale",
-    href: "https://linkedin.com/in/muizzyrankin",
-  },
-  twitter: {
-    name: "Twitter / X",
-    handle: "@muizzyranking",
-    href: "https://twitter.com/muizzyranking",
-  },
-};
+import { site } from "@/lib/site";
 
-export const EMAIL = "oyebowaleabdulmuiz@gmail.com";
+export const SOCIALS = {
+  github: { name: "GitHub", handle: "@muizzyranking", href: "https://github.com/Muizzyranking" },
+  linkedin: { name: "LinkedIn", handle: "Muiz Oyebowale", href: "https://linkedin.com/in/muizzyrankin" },
+  twitter: { name: "Twitter / X", handle: "@muizzyranking", href: "https://twitter.com/muizzyranking" },
+} as const;
+
+export const EMAIL = site.email;
--- src/lib/font.ts
+++ src/lib/font.ts
@@ -1,16 +1,23 @@
-import { JetBrains_Mono, Space_Grotesk } from "next/font/google";
+import { Fraunces, IBM_Plex_Mono, Poppins } from "next/font/google";
 
-export const spaceGrotesk = Space_Grotesk({
+export const poppins = Poppins({
   subsets: ["latin"],
   display: "swap",
-  weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-space-grotesk",
+  weight: ["400", "500", "600"],
+  variable: "--font-poppins",
 });
 
-export const jetbrainsMono = JetBrains_Mono({
+export const fraunces = Fraunces({
   subsets: ["latin"],
   display: "swap",
-  weight: ["300", "400", "500", "700"],
-  style: ["normal", "italic"],
-  variable: "--font-jetbrains-mono",
+  variable: "--font-fraunces",
 });
+
+export const plexMono = IBM_Plex_Mono({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "500", "600"],
+  variable: "--font-plex-mono",
+});
+
+export const fontVariables = `${poppins.variable} ${fraunces.variable} ${plexMono.variable}`;
